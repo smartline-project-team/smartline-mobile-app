ab662b3bbeb05ada49c5d0401ffd1d96e5dbaa46
LeftTry <lerest.go@gmail.com>
2024-11-30 03:03:51 +0600

    [Feature] Change App interface

--- smartline-mobile-app/composeApp/src/commonMain/kotlin/org/smartline/app/App.kt
+++ smartline-mobile-app/composeApp/src/commonMain/kotlin/org/smartline/app/App.kt
@@ -1,28 +1,39 @@
 package org.smartline.app
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.smartline.app.views.LoginView
-import org.smartline.app.views.RegisterView
+import org.smartline.app.views.auth.AuthView
+import org.smartline.app.views.auth.ConfirmEmailView
 
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        val showContent = remember { mutableStateOf(false) }
-        val showLogin = remember { mutableStateOf(true) }
-        val showRegister = remember { mutableStateOf(false) }
+        val showContent = remember { mutableStateOf(true) }
+        val showConfirmation = remember { mutableStateOf(false) }
         Box( modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            LoginView(showLogin = showLogin, showRegister = showRegister, showContent = showContent)
-            RegisterView(showLogin = showLogin, showRegister = showRegister, showContent = showContent)
+            //LoginView(showLogin = showLogin, showRegister = showRegister, showContent = showContent)
+            //RegisterView(showLogin = showLogin, showRegister = showRegister, showContent = showContent)
+            Column(
+                modifier = Modifier.fillMaxWidth(0.8f),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                AuthView(showContent = showContent, showConfirmation = showConfirmation)
+                ConfirmEmailView(showContent = showConfirmation)
+            }
         }
     }
 }
