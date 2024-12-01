baf5eb58416d6f8192abfa4921e0e61705f79c9b
LeftTry <lerest.go@gmail.com>
2024-12-01 15:33:08 +0600

    [Minor] Small fixes

--- smartline-mobile-app/composeApp/src/commonMain/kotlin/org/smartline/app/views/start/WelcomeView.kt
+++ smartline-mobile-app/composeApp/src/commonMain/kotlin/org/smartline/app/views/start/WelcomeView.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
@@ -31,6 +32,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -87,8 +89,9 @@ fun WelcomeView(next: MutableState<String>, currentLanguage: MutableState<String
 
     Box(
         modifier = Modifier
-            .fillMaxSize() // Ensures the parent Box takes the full available size
+            .fillMaxSize()
     ) {
+
     Box(
         modifier = Modifier
             .alpha(alphaButton.value)
@@ -98,6 +101,7 @@ fun WelcomeView(next: MutableState<String>, currentLanguage: MutableState<String
             .background(Color.LightGray)
             .clickable { expanded = true }
             .padding(8.dp)
+
     ) {
         Row(
             modifier = Modifier.wrapContentSize(),
