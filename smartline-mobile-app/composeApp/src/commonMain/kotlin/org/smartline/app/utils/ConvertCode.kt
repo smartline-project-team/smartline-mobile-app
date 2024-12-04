5ccbfa2144ccbc63ac644631744597e2d19571ef
LeftTry <lerest.go@gmail.com>
2024-12-04 15:16:15 +0600

    [Minor] Change code conversion to more determined format

---
+++ smartline-mobile-app/composeApp/src/commonMain/kotlin/org/smartline/app/utils/ConvertCode.kt
@@ -0,0 +1,9 @@
+package org.smartline.app.utils
+
+fun ConvertCode(code: List<String>) : String {
+    var result = ""
+    for (element in code) {
+        result += element
+    }
+    return result
+}
