283cbd7b3d8e5a706fe04db435910b06bfea0224
LeftTry <lerest.go@gmail.com>
2024-12-04 15:15:34 +0600

    [Minor] Provide design

--- smartline-mobile-app/composeApp/src/commonMain/kotlin/org/smartline/app/views/start/WelcomeView.kt
+++ smartline-mobile-app/composeApp/src/commonMain/kotlin/org/smartline/app/views/start/WelcomeView.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
@@ -86,6 +89,10 @@ fun WelcomeView(next: MutableState<String>, currentLanguage: MutableState<String
                 onClick = { next.value = "authScreen" },
                 modifier = Modifier
                     .alpha(alphaButton.value)
+                    .fillMaxWidth(0.6f)
+                    .height(48.dp),
+                shape = RoundedCornerShape(16.dp),
+                colors = ButtonDefaults.buttonColors(KColor.primary)
             ) {
                 Text(text = welcomeButtonText, color = KColor.background,
                     fontSize = 16.sp, fontFamily = FontFamily(Font(Res.font.Jost)))
